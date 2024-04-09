c3ea17fb83a711dac48eb1d0aa0cb9fce8787e7e
8BitGinger <ryan.fann>
2024-04-09 14:51:31 -0500

    push working app

---
+++ README.md
@@ -0,0 +1,22 @@
+# AI Image Generator
+
+This is a beginner-friendly AI tool built using OpenAI's API. It represents my first exploration into creating software powered by artificial intelligence. I'm excited to share it and learn from your feedback!
+
+## Screenshots
+
+Homepage:
+
+![main](./src/Components/Assets/aiHomepage.png)
+
+Example of Generation:
+
+![generate](./src/Components/Assets/aiExample.png)
+
+Example on Mobile:
+![mobile](./src/Components/Assets/aiMainMobile.png)
+
+# Contact
+
+[My Portfolio](https://ryanfann.netlify.app/)
+
+[My GitHub](https://github.com/8BitGinger)
---
+++ client/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,75 @@
+import React, { useRef, useState } from 'react';
+import './ImageGenerator.css';
+import default_image from '../../Components/Assets/aiPlaceholder.png';
+
+export const ImageGenerator = () => {
+  const [image_url, setImage_url] = useState('/');
+  const [loading, setLoading] = useState(false);
+  let inputRef = useRef(null);
+
+  const imageGenerator = async () => {
+    if (inputRef.current.value === '') {
+      return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+      'https://api.openai.com/v1/images/generations',
+      {
+        method: 'Post',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer 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',
+          'User-Agent': 'Chrome',
+        },
+        body: JSON.stringify({
+          prompt: inputRef.current.value,
+          n: 1,
+          size: '512x512',
+        }),
+      }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
+  };
+
+  return (
+    <div className="ai-image-generator">
+      <div className="header">
+        AI Image <span>Generator</span>
+      </div>
+      <div className="img-loading">
+        <div className="image">
+          <img
+            src={image_url === '/' ? default_image : image_url}
+            alt="placeholder"
+          />
+        </div>
+        <div className="loading">
+          <div className={loading ? 'loading-bar-full' : 'loading-bar'}></div>
+          <div className={loading ? 'loading-text' : 'display-none'}>
+            Loading...
+          </div>
+        </div>
+      </div>
+      <div className="search-box">
+        <textarea
+          className="search-input"
+          type="text"
+          ref={inputRef}
+          placeholder="Describe What You Want to See!"
+        />
+        <div
+          className={loading ? 'button-disabled' : 'generate-btn'}
+          onClick={() => {
+            imageGenerator();
+          }}
+        >
+          Generate
+        </div>
+      </div>
+    </div>
+  );
+};
