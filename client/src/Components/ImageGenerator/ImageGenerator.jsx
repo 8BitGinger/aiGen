ded62e36529289a2d72378fee117b5d2ff2f131f
8BitGinger <ryan.fann>
2024-04-09 15:04:00 -0500

    push working app

--- client/src/Components/ImageGenerator/ImageGenerator.jsx
+++ client/src/Components/ImageGenerator/ImageGenerator.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from 'react';
 import './ImageGenerator.css';
 import default_image from '../../Components/Assets/aiPlaceholder.png';
 
+const API_KEY = 'Bearer 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';
+
 export const ImageGenerator = () => {
   const [image_url, setImage_url] = useState('/');
   const [loading, setLoading] = useState(false);
@@ -12,23 +14,20 @@ export const ImageGenerator = () => {
       return 0;
     }
     setLoading(true);
-    const response = await fetch(
-      'https://api.openai.com/v1/images/generations',
-      {
-        method: 'Post',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization:
-            'Bearer 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',
-          'User-Agent': 'Chrome',
-        },
-        body: JSON.stringify({
-          prompt: inputRef.current.value,
-          n: 1,
-          size: '512x512',
-        }),
-      }
-    );
+    const response = await fetch(API_KEY, {
+      method: 'Post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer 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',
+        'User-Agent': 'Chrome',
+      },
+      body: JSON.stringify({
+        prompt: inputRef.current.value,
+        n: 1,
+        size: '512x512',
+      }),
+    });
     let data = await response.json();
     let data_array = data.data;
     setImage_url(data_array[0].url);
